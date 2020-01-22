6f9ede9ae78d0ac11a8ae8949dd5541742f391a9
Arman Aryanpour <v2jy@yahoo.com>
2020-01-22 15:07:36 -0500

    different search approach

--- src/components/index.component.js
+++ src/components/index.component.js
@@ -8,7 +8,7 @@ import Navbar from "../components/navbar.component";
 import { DashboardPortfolios } from "./user/functions/custom_queries";
 import * as mutations from "../graphql/mutations";
 import navbarSettings from "../settings/navbar";
-
+import ActiveList from "./main/activelist.component.js"
 class Index extends Component {
   constructor(props) {
     super(props);
@@ -29,25 +29,7 @@ class Index extends Component {
   async componentDidMount() {
         this.updateWindowDimensions();
         window.addEventListener("resize", this.updateWindowDimensions.bind(this));
-    // Most Active Fetch
-    try {
-      var data = await fetch(
-        "https://financialmodelingprep.com/api/v3/stock/actives"
-      );
-      var data = await data.json();
-      // Most Gainer Fetch
-      var dataG = await fetch(
-        "https://financialmodelingprep.com/api/v3/stock/gainers"
-      );
-      var dataG = await dataG.json();
-      console.log(data);
-      console.log(dataG);
 
-      this.setState({
-        activeList: data.mostActiveStock,
-        gainerList: dataG.mostGainerStock
-      });
-    } catch (error) {}
   }
   wait = ms => {
     var start = new Date().getTime();
@@ -84,8 +66,8 @@ class Index extends Component {
       return (
         <div className="wrap">
           <Navbar data={this.props.data}></Navbar>
-          <div className="container-fluid main ">
-            <div className="overlay-phone  mx-0">
+          <div className="container-fluid row mx-0 main ">
+            <div className="overlay-phone Gscroll col-12 mx-0">
               <div className="row">
                 <div className="col-12 text-white ">
                   <h1 class="display-4 main-heading px-5 pt-3 pt-lg-5">
@@ -114,46 +96,16 @@ class Index extends Component {
                 </div>
                 <div className="col-12 pt-lg-5 row mx-0 text-white d-none d-lg-block">
                   <div className="col-6  ml-auto pr-5 ">
-                    <div className="row mx-0 mainbg px-4 py-4 rounded">
-                      <div className="col-12 mx-0">
-                        <h5>Today's Most Active Performers</h5>
-                      </div>
-                      {this.state.activeList &&
-                        this.state.activeList.map(item => (
-                          <div className="col-lg-4  col-6 py-3 px-2 px-lg-3 ">
-                                                     <a href={`/equ/${item.ticker}`}>
-                            <div className={item.changes > 0 ? "card text-white bg-success " : item.changes < 0 ? "card text-white bg-danger " : "card text-white bg-secondary "}>
-                              <div className="card-header row mx-0 p-1 py-2 text-center">
-                                {/* MAIN SIGN */}
-                                <div className="col-12 py-1 px-0 d-flex justify-content-around ">
-                                  {" "}
-                                 
-                                  <span className="badge badge-light text-dark badge-pill ">
-                                    {item.ticker}
-                                  </span>
-                                  <p className="m-0 p-0 d-inline smallerText">
-                                    {"$" + item.price}
-                                  </p>
-                                </div>
-                                <div className="col-12  px-0 d-flex justify-content-around ">
-                                  {" "}
-                                  <p className="m-0 p-0 d-inline smallerText">
-                                    {item.changes}
-                                  </p>
-                                  <p className="m-0 p-0 d-inline smallerText">
-                                    {"$" + item.changesPercentage}
-                                  </p>
-                                </div>
-                              </div>
-                              {/* dumping and expanding information about selected stocks */}
-                            </div></a>
-                          </div>
-                        ))}
-                    </div>
+                                      <ActiveList/>
                   </div>
                 </div>
               </div>
             </div>
+             <div className="row col-12 mx-0 text-white px-0 d-lg-none">
+                  <div className="col-12 px-0 ">
+                  <ActiveList/>
+                  </div>
+                </div>
           </div>
         </div>
       );
---
+++ src/components/main/activelist.component.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+
+export default class dumper extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  async componentDidMount() {
+    try {
+      var data = await fetch(
+        "https://financialmodelingprep.com/api/v3/stock/actives"
+      );
+      var data = await data.json();
+      // Most Gainer Fetch
+      var dataG = await fetch(
+        "https://financialmodelingprep.com/api/v3/stock/gainers"
+      );
+      var dataG = await dataG.json();
+      console.log(data);
+      console.log(dataG);
+
+      this.setState({
+        activeList: data.mostActiveStock,
+        gainerList: dataG.mostGainerStock
+      });
+    } catch (error) {}
+  }
+  render() {
+    return (
+      <div className="row mx-0 mainbg px-4 py-4 rounded">
+        <div className="col-12 mx-0">
+          <h5>Today's Most Active Performers</h5>
+        </div>
+        {this.state.activeList &&
+          this.state.activeList.map(item => (
+            <div className="col-lg-4  col-6 py-3 px-2 px-lg-3 ">
+              <a href={`/equ/${item.ticker}`}>
+                <div
+                  className={
+                    item.changes > 0
+                      ? "card text-white bg-success "
+                      : item.changes < 0
+                      ? "card text-white bg-danger "
+                      : "card text-white bg-secondary "
+                  }
+                >
+                  <div className="card-header row mx-0 p-1 py-2 text-center">
+                    {/* MAIN SIGN */}
+                    <div className="col-12 py-1 px-0 d-flex justify-content-around ">
+                      {" "}
+                      <span className="badge badge-light text-dark badge-pill ">
+                        {item.ticker}
+                      </span>
+                      <p className="m-0 p-0 d-inline smallerText">
+                        {"$" + item.price}
+                      </p>
+                    </div>
+                    <div className="col-12  px-0 d-flex justify-content-around ">
+                      {" "}
+                      <p className="m-0 p-0 d-inline smallerText">
+                        {item.changes}
+                      </p>
+                      <p className="m-0 p-0 d-inline smallerText">
+                        {"$" + item.changesPercentage}
+                      </p>
+                    </div>
+                  </div>
+                  {/* dumping and expanding information about selected stocks */}
+                </div>
+              </a>
+            </div>
+          ))}
+
+          {/* Gainer Dump */}
+
+          <div className="col-12 mx-0">
+          <h5>Today's Most Gainer Performers</h5>
+        </div>
+        {this.state.activeList &&
+          this.state.gainerList.map(item => (
+            <div className="col-lg-4  col-6 py-3 px-2 px-lg-3 ">
+              <a href={`/equ/${item.ticker}`}>
+                <div
+                  className={
+                    item.changes > 0
+                      ? "card text-white bg-success "
+                      : item.changes < 0
+                      ? "card text-white bg-danger "
+                      : "card text-white bg-secondary "
+                  }
+                >
+                  <div className="card-header row mx-0 p-1 py-2 text-center">
+                    {/* MAIN SIGN */}
+                    <div className="col-12 py-1 px-0 d-flex justify-content-around ">
+                      {" "}
+                      <span className="badge badge-light text-dark badge-pill ">
+                        {item.ticker}
+                      </span>
+                      <p className="m-0 p-0 d-inline smallerText">
+                        {"$" + item.price}
+                      </p>
+                    </div>
+                    <div className="col-12  px-0 d-flex justify-content-around ">
+                      {" "}
+                      <p className="m-0 p-0 d-inline smallerText">
+                        {item.changes}
+                      </p>
+                      <p className="m-0 p-0 d-inline smallerText">
+                        {"$" + item.changesPercentage}
+                      </p>
+                    </div>
+                  </div>
+                  {/* dumping and expanding information about selected stocks */}
+                </div>
+              </a>
+            </div>
+          ))}
+      </div>
+    );
+  }
+}
